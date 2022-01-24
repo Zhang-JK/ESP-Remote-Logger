e45271b4e6e0bf94ebc82f848317e53f01d667e5
Zhang Jiekai <zjkisgood@126.com>
2022-01-25 01:07:13 +0800

    add CAN receive transmit module

---
+++ 2. Firmware/ESP-Remote-Logger/include/can.h
@@ -0,0 +1,34 @@
+#ifndef CAN_H
+#define CAN_H
+
+#include <ESP32CAN.h>
+#include <CAN_config.h>
+#include <string.h>
+
+#ifndef CAN_MESSAGE_LENGTH
+#define CAN_MESSAGE_LENGTH 8
+#endif
+
+class CAN 
+{
+   private:
+    CAN_frame_t rx_frame;
+    CAN_frame_t tx_frame;
+
+    uint8_t rxData[CAN_MESSAGE_LENGTH];
+    uint8_t txData[CAN_MESSAGE_LENGTH];
+    int rxID;
+    int txID;
+
+   public:
+    uint8_t *getRxData() { return rxData; }
+    void setTxData(uint8_t *data) { memcpy(txData, data, CAN_MESSAGE_LENGTH); }
+    int getRxID() { return rxID; }
+    void setTxID(int id) { txID = id; }
+    
+    void init(CAN_speed_t speed, gpio_num_t rx, gpio_num_t tx);
+    bool receive(int timeout);
+    bool transmit();
+};
+
+#endif
--- 2. Firmware/ESP-Remote-Logger/src/main.cpp
+++ 2. Firmware/ESP-Remote-Logger/src/main.cpp
@@ -1,9 +1,11 @@
 #include <Arduino.h>
 #include <network.h>
 #include <password.h>
+#include <can.h>
 
 Network wifi;
 WiFiServer server;
+CAN can;
 
 void setup() {
   // start debug serial port
@@ -13,6 +15,8 @@ void setup() {
   wifi.init(SSID, USER_NAME, PASSWORD);
   server.begin(23333);
 
+  // init CAN receive
+  can.init(CAN_SPEED_1000KBPS, GPIO_NUM_32, GPIO_NUM_33);
 }
 
 void loop() {
@@ -20,4 +24,22 @@ void loop() {
   WiFiClient client = server.available();
   if(client.connected())
     client.println("Data Test");
+
+  // read motor data
+  if (can.receive(1)) {
+    Serial.printf("Received from 0x%08X: ", can.getRxID());
+    for (int i = 0; i < CAN_MESSAGE_LENGTH; i++) {
+      Serial.print(can.getRxData()[i]);
+      Serial.print(" ");
+    }
+    Serial.println("");
+  }
+
+  // send motor data
+  uint8_t data[CAN_MESSAGE_LENGTH] = {0x00, 0x00, 0x0A, 0x00, 0x00, 0x00, 0x00, 0x00};
+  can.setTxData(data);
+  can.setTxID(0x200);
+  can.transmit();
+
+  delay(50);
 }
